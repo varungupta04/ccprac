cf8ec32ca9ea44bb518ed526fbf83364c59f525c
varungupta04 <87354396+varungupta04@users.noreply.github.com>
2024-08-03 00:46:56 +0530

    Time: 3 ms (70.49%), Space: 11.4 MB (29.54%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string st = "";
-        sort(strs.begin(),strs.end());
-        int a = strs.size();
-        string n = strs[0];
-        string m = strs[a-1];
-        for(int i=0;i<n.size();i++)
-        {
-          if(n[i]==m[i])
-          {st += n[i];}
-          else
-          { break; }
+        if (strs.empty()) return ""; // Handle the empty input case
+
+        // Sort the strings
+        sort(strs.begin(), strs.end());
+        
+        // After sorting, compare the first and last string
+        string a = strs[0];
+        string b = strs[strs.size() - 1];
+        string ans = "";
+
+        // Compare characters of the first and last string
+        for (int i = 0; i < a.length(); i++) {
+            if (a[i] == b[i]) {
+                ans += a[i];
+            } else {
+                break; // Stop when characters don't match
+            }
         }
-        return st;
+
+        return ans;
     }
-    
 };
