3b7b9bd9f495df48410302b0f26636328b0e57c8
varungupta04 <87354396+varungupta04@users.noreply.github.com>
2024-08-07 21:57:38 +0530

    Time: 14 ms (73.95%), Space: 17.8 MB (32.56%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,63 @@
+//Approach-1 (Using BFS Cycle Check - Kahn's Algorithm (Topological Sort)
+class Solution {
+public:
+    
+    bool topologicalSortCheck(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree) {
+        queue<int> que;
+        
+        int count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            if(indegree[i] == 0) {
+                count++;
+                que.push(i);
+            }
+        }
+        
+        
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+            
+            for(int &v : adj[u]) {
+                
+                indegree[v]--;
+                
+                if(indegree[v] == 0) {
+                    count++;
+                    que.push(v);
+                }
+                
+            }
+            
+        }
+        
+        if(count == n) //I was able to visit all nodes (course)
+            return true; //i.e. I was able to finish all courses
+        
+        return false; //means there was a cycle and I couldn't complete all courses
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        unordered_map<int, vector<int>> adj;
+        
+        vector<int> indegree(numCourses, 0); //kahn's algo
+        
+        for(auto &vec : prerequisites) {
+            int a = vec[0];
+            int b = vec[1];
+            
+            //b ---> a
+            adj[b].push_back(a);
+            
+            //arrow ja raha hai 'a' me
+            indegree[a]++;
+        }
+        
+        
+        //if cycle is present, not possible
+        
+        return topologicalSortCheck(adj, numCourses, indegree);
+    }
+};
