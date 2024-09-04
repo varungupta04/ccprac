4a5e4f8551983f1d0d6d6e52dc3b6693ff4aec56
varungupta04 <87354396+varungupta04@users.noreply.github.com>
2024-09-04 17:13:29 +0530

    Time: 376 ms (98.56%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
 
-SELECT name FROM Customer WHERE referee_id != 2 or referee_id is null;
+select name from customer where referee_id != 2 or referee_id is null;
+
+
+-- main thing is using is null
